b856bd38bff1247a03d692c5b7a9b4a2b6e8e887
Fadab2 <ali_berg1@yahoo.com>
2022-04-07 01:05:32 -0700

    added table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
@@ -11,27 +12,46 @@ const Home: NextPage = () => {
       </Head>
       <header className='flex items-center justify-between p-4 bg-emerald-500'>
         <h1 className='text-4xl'>Cookie Stand Admin</h1>
+      
       </header>
       <main className="">
-        <div className='bg-emerald-200'>
+        <div className='justify-center bg-emerald-200'>
         <legend className='flex items-center text-xl'>Create Cookie Stand</legend>
         <form className='flex my-4 bg-emerald-200'>
           
-            <label className="flex">Location
+            <label> <div className="mt-1.5">Location</div> 
               <input type="text"/>
             </label>
          
-            <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase">
-              Minimum Customers per Hour
-            </label>
-            <label><span className="text-sm text-center">Maximum Customers per Hour</span>
+             <label><span className="text-sl">Minimum Customers per Hour</span>
               <input type="number"
                />
             </label>
-          <input name="create"></input>
+            <label><span className="text-sl">Maximum Customers per Hour</span>
+              <input type="number"
+               />
+            </label>
+              <label><span className="text-sl">Average Cookies per Sale</span>
+              <input type="number"
+               />
+            </label>
           <button className="flex items-center justify-between p-4 bg-emerald-500">Create</button>
           </form>
-          </div>
+        </div>
+        <table className='w-1/2 mx-auto my-4'>
+          <thead>
+            <tr>
+              <th className='border-gray-700'>Minimum Customers per Hour</th>
+              <th className='border-gray-700'>Maximum Customers per Hour</th>
+              <th className='border-gray-700'>Average Cookies per Sale</th>
+            </tr>
+          </thead>
+          <tbody>
+            <td className='pl-2 border border-gray-700'>2</td>
+            <td className='pl-2 border border-gray-700'>4</td>
+            <td className='pl-2 border border-gray-700'>2.5</td>
+          </tbody>
+        </table>
       </main>
 
       <footer className="flex items-center justify-between p-4 bg-emerald-500">
